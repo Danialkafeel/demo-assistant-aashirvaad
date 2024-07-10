2d4c9bc9529f2a849f1bb96e962036988fb946b9
danialkafee <danialkafee@adobe.com>
2024-07-10 12:42:21 +0530

    Add accepted blocks from AEM Assistant

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,29 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
+  // Add a class to the block
+  block.classList.add('product-block');
+
+  // Select all card elements and style them
+  const cards = block.querySelectorAll('.cards > div');
+  cards.forEach((card) => {
+    const imgContainer = card.querySelector('div:first-child');
+    const linkContainer = card.querySelector('div:last-child');
+    const imgElement = imgContainer.querySelector('img');
+    const linkElement = linkContainer.querySelector('a');
+
+    // Add required class to each card
+    card.classList.add('product-card');
+
+    // Add required class to img container
+    imgContainer.classList.add('product-img-container');
+
+    // Add required class to link container
+    linkContainer.classList.add('product-link-container');
+
+    // Add required class to images
+    imgElement.classList.add('product-img');
+
+    // Add required class to links
+    linkElement.classList.add('product-link');
   });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
-  block.append(ul);
 }
